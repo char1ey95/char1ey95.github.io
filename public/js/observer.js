230079962f954e1e23cdbff40046fcbc16f02eab
char1ey <ckstn410@gmail.com>
2022-12-02 21:27:46 +0900

    221102 팝업창 띄우기 남았음

--- public/js/observer.js
+++ public/js/observer.js
@@ -1,39 +1,64 @@
-let article = document.querySelectorAll('article')
-let project = document.querySelectorAll('.project_board')
+const observer_opacity = document.querySelectorAll('.observer_opacity')
 
 let observer = new IntersectionObserver((e)=>{
     e.forEach((box) => {
         if(box.isIntersecting){
             box.target.style.opacity = 1;
+            box.target.style.bottom = 0;
+            box.target.style.padding = "2.5rem";
         } else {
             box.target.style.opacity = 0;
+            box.target.style.bottom = "-3rem";
+            box.target.style.padding = "2rem";
             // box.target.style.background = tranparent;
         }
     })
 })
 
-observer.observe(article[0])
-observer.observe(article[1])
-observer.observe(article[2])
+for(let i = 0; i < observer_opacity.length; i++){
+    observer.observe(observer_opacity[i])
+}
+
+
+// ===========================================================================
+
+
+const observer_opacity_2 = document.querySelectorAll('.observer_opacity_2')
+
+let observer_2 = new IntersectionObserver((e)=>{
+    e.forEach((box) => {
+        if(box.isIntersecting){
+            box.target.style.opacity = 1;
+            box.target.style.bottom = 0;
+        } else {
+            box.target.style.opacity = 0;
+            box.target.style.bottom = "-3rem";
+        }
+    })
+})
+
+for(let i = 0; i < observer_opacity_2.length; i++){
+    observer_2.observe(observer_opacity_2[i])
+}
+
+
+// ===========================================================================
+
+
+const project = document.querySelectorAll('.project_board')
 
 let observer_proj = new IntersectionObserver((e)=>{
     e.forEach((box) => {
         if(box.isIntersecting){
-            box.target.style.left = 0;
+            box.target.style.opacity = 1;
+            box.target.style.bottom = 0;
         } else {
-            box.target.style.left = "-800px";
+            box.target.style.opacity = 0;
+            box.target.style.bottom = "-3rem";
         }
-        // if(box.isIntersecting){
-        
-        // } else {
-        //     box.target.style.left = "-600px";
-        // }
     })
-},{
-    threshold:0.1
 })
-observer_proj.observe(project[0])
-observer_proj.observe(project[1])
-observer_proj.observe(project[2])
-observer_proj.observe(project[3])
-observer_proj.observe(project[4])
+
+for(let i = 0; i < project.length; i++){
+    observer_proj.observe(project[i])
+}
