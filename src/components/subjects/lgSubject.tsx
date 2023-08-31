3a5d135e5988d0a171c7674462e1dfbdc6aeb6e6
char1ey <ckstn410@gmail.com>
2023-08-31 18:58:42 +0900

    Feat: mSubject 컴포넌트 생성

--- src/components/subjects/lgSubject.tsx
+++ src/components/subjects/lgSubject.tsx
@@ -1,6 +1,6 @@
 export default function LgSubject({ children }: any) {
     return (
-        <div className="flex py-[0.625rem] px-0 justify-center items-center gap-2.5 self-stretch hero_font">
+        <div className="flex py-[0.625rem] px-0 justify-center items-center gap-2.5 self-stretch text-center text-5xl font-normal leading-normal">
             {children}
         </div>
     )
