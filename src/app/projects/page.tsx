aa10ca4c158f55e31e76ab9073320b1f36abbc17
char1ey <ckstn410@gmail.com>
2023-08-31 19:15:09 +0900

    Feat: Project 헤딩 완성

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,14 @@
+import SubjectSeparator from "@/components/line/subjectSeparator"
+import MSubject from "@/components/subjects/mSubject"
+
 const Projects = () => {
     return (
-        <section>
-            Project
-        </section>
+        <div className="flex py-6 px-40 flex-col items-center gap-6 self-stretch">
+            <div className="flex flex-col items-center">
+                <MSubject>Project</MSubject>
+                <SubjectSeparator />
+            </div>
+        </div>
     )
 }
 
