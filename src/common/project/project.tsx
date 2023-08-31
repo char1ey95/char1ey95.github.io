3818ce7d0bc92fdab30a0b38526fce02cefe2360
char1ey <ckstn410@gmail.com>
2023-08-31 20:11:02 +0900

    Feat: ProjectWrap 생성

---
+++ src/common/project/project.tsx
@@ -0,0 +1,14 @@
+export default function Project() {
+    return (
+        <div className="flex w-[68.8125rem] flex-col justify-center items-center gap-[4.6875rem]">
+
+        </div>
+    )
+}
+
+// display: flex;
+// width: 68.8125rem;
+// flex-direction: column;
+// justify-content: center;
+// align-items: center;
+// gap: 4.6875rem;
