afd54785c0e038feea9dd8a2b09c1a8d05818530
char1ey <ckstn410@gmail.com>
2023-09-04 22:56:37 +0900

    Feat: Stack 박스 추가

---
+++ src/components/boxes/stackImageBox.tsx
@@ -0,0 +1,7 @@
+export default function StackImageBox({ children }: { children: any }) {
+    return (
+        <div className="flex justify-center items-center w-2/5 h-full">
+            {children}
+        </div>
+    )
+}
---
+++ src/components/boxes/stackTextBox.tsx
@@ -0,0 +1,7 @@
+export default function StackTextBox({ children }: { children: any }) {
+    return (
+        <div className="flex flex-col justify-center items-center w-3/5">
+            {children}
+        </div>
+    )
+}
