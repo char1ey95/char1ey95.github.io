b0ee1c990df1eeea42ca03127f613c7be904baed
char1ey <ckstn410@gmail.com>
2023-09-04 22:55:58 +0900

    Feat: shadow 커스텀 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        "custom": 'rgba(0, 0, 0, 0.02) 2px 2px 4px;'
+      }
     },
   },
   plugins: [],
