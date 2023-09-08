2431b8951d80afde12a032fce7f111f51a07b415
char1ey <ckstn410@gmail.com>
2023-09-08 15:06:15 +0900

    Fixed: About 레이아웃

--- src/common/about/about.tsx
+++ src/common/about/about.tsx
@@ -12,11 +12,11 @@ export default function About() {
         <div className="mt-16 xl:mt-0 xl:h-[60rem] flex flex-col justify-center items-center">
             <MSubject>About</MSubject>
             <Separator />
-            <div className="my-4 py-4 xl:py-8 flex flex-col xl:flex-row justify-center xl:justify-around items-center rounded-lg w-80 min-[480px]:w-96 sm:w-[30rem] md:w-[36rem] lg:w-[42rem] xl:w-[66rem] 2xl:w-[72rem] bg-stone-200 dark:bg-stone-800">
+            <div className="mx-4 my-4 px-8 py-4 xl:py-8 flex flex-col xl:flex-row justify-center xl:justify-around items-center rounded-lg md:w-[45rem] lg:w-[42rem] xl:w-[66rem] 2xl:w-[72rem] bg-stone-200 dark:bg-stone-800">
                 <div className="mx-auto xl:mx-0 my-4 xl:my-0 w-64 sm:w-80 md:w-96 xl:w-1/4">
                     <Image src={photo} alt="" className="rounded-xl mx-auto" />
                 </div>
-                <div className="flex flex-col w-72 sm:w-96 lg:w-[30rem] xl:w-3/5">
+                <div className="flex flex-col lg:w-[30rem] xl:w-3/5">
                     <h1 className="my-2 md:my-4 text-4xl text-center font-bold">강찬수</h1>
                     <div className="my-2 text-lg md:text-xl lg:text-2xl text-center font-semibold">
                         <p>함께 성장하는 <br className="sm:hidden" /> 개발자가 되고싶습니다</p>
