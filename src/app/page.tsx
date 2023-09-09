82be7ee10344b7bc235606abf3d9be4bd543207f
root <root@Char1ey>
2023-09-10 04:23:21 +0900

    Feat: UpBtn 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import About from "@/common/about/about"
 import Hero from "@/common/hero/hero"
 import Project from "@/common/project/project"
 import Skill from "@/common/skill/skill"
-import { DATABASE_ID, options } from "@/config"
 
 export default async function Home() {
 
   return (
-    <div className="mt-16">
+    <div className="mt-16 relative">
       <Hero />
       <About />
       <Skill />
---
+++ src/components/button/upBtn.tsx
@@ -0,0 +1,7 @@
+export default function UpBtn() {
+    return (
+        <button className="fixed animate-pulse bottom-6 right-12 p-1 h-12 w-12 text-4xl text-center bg-[#2C3333] hover:animate-none hover:bg-[#495555] rounded-full text-slate-50">
+            ↑
+        </button>
+    )
+}
